698c99d25b02772e13ae3524a59995773b29671b
Sebastian Duque <sebas_tian_95@hotmail.com>
2016-03-19 23:22:16 -0500

    Migrate client module

---
+++ client/app.js
@@ -0,0 +1,61 @@
+const domify = require('domify');
+const Webrtc2Images = require('webrtc2images');
+const uuid = require('uuid');
+const messageTpl = require('./templates/message.hbs');
+const io = require('socket.io-client');
+
+const socket = io.connect();
+const id = uuid.v4();
+
+const rtc = new Webrtc2Images({
+  width: 200,
+  height: 200,
+  frames: 10,
+  type: "image/jpeg",
+  quality: 0.8,
+  interval: 200
+});
+
+rtc.startVideo(function(err) {
+  if (err) return logError(err);
+});
+
+const messages = document.querySelector('#messages');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  record();
+}, false);
+
+socket.on('message', addMessage);
+
+socket.on('messageack', function(message) {
+  if (message.id === id) {
+    addMessage(message);
+  }
+});
+
+socket.on('messages', function(messages) {
+  messages.forEach(addMessage);
+});
+
+function record() {
+  const input = document.querySelector('input[name="message"]');
+  const message = input.value;
+  input.value = "";
+
+  rtc.recordVideo(function(err, frames) {
+    if (err) logError(err);
+    socket.emit('message', {id: id, message: message, frames: frames});
+  });
+}
+
+function addMessage(message) {
+  const m = messageTpl(message);
+  messages.appendChild(domify(m));
+}
+
+function logError(err) {
+  console.error(err);
+}
